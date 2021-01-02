ee4f045eab14a41f20b8141684c94fd220284baa
kenmueller <kenmueller0@gmail.com>
2021-01-02 01:04:54 -0800

    removed pwa

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
 module.exports = require('next-compose-plugins')([
 	[require('next-optimized-images')],
-	[require('@next/mdx')()],
-	[require('next-pwa'), {
-		pwa: {
-			disable: process.env.NODE_ENV === 'development',
-			dest: 'public',
-			runtimeCaching: require('next-pwa/cache')
-		}
-	}]
+	[require('@next/mdx')()]
 ])
