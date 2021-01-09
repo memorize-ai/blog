a077b15f54292c700fcade05c65ec1239ab1cc16
Aditya Rawat <aditya1rawat@gmail.com>
2021-01-08 20:51:05 -0800

    cleaned up comments

--- components/PostPreview.tsx
+++ components/PostPreview.tsx
@@ -7,13 +7,10 @@ import styles from 'styles/components/PostPreview.module.scss';
 
 const PostPreview = ({ post }: { post: Post }) => (
   <Link href='/p/[slug]' as={`/p/${post.slug}`}>
-    {/* <a  */}
     <div className={styles.root}>
       <PostHeader allowLinks={false} post={post} />
-      {/* <PostBody className={styles.body} allowLinks={false} post={post} /> */}
       <p className={styles.readMore}>Read more...</p>
     </div>
-    {/* </a> */}
   </Link>
 );
 
