ad0e958ea0945a838fb322ab21167381201924e5
Aditya Rawat <aditya1rawat@gmail.com>
2021-01-08 18:48:24 -0800

    removed webpack and styled blog preview

--- components/PostPreview.tsx
+++ components/PostPreview.tsx
@@ -1,25 +1,21 @@
-import Link from 'next/link'
+import Link from 'next/link';
 
-import Post from 'models/Post'
-import PostHeader from './PostHeader'
-import PostBody from './PostBody'
+import Post from 'models/Post';
+import PostHeader from './PostHeader';
+import PostBody from './PostBody';
 
-import styles from 'styles/components/PostPreview.module.scss'
+import styles from 'styles/components/PostPreview.module.scss';
 
 const PostPreview = ({ post }: { post: Post }) => (
-	<Link href="/p/[slug]" as={`/p/${post.slug}`}>
-		<a className={styles.root}>
-			<PostHeader allowLinks={false} post={post} />
-			<PostBody
-				className={styles.body}
-				allowLinks={false}
-				post={post}
-			/>
-			<p className={styles.readMore}>
-				Read more...
-			</p>
-		</a>
-	</Link>
-)
+  <Link href='/p/[slug]' as={`/p/${post.slug}`}>
+    {/* <a  */}
+    <div className={styles.root}>
+      <PostHeader allowLinks={false} post={post} />
+      {/* <PostBody className={styles.body} allowLinks={false} post={post} /> */}
+      <p className={styles.readMore}>Read more...</p>
+    </div>
+    {/* </a> */}
+  </Link>
+);
 
-export default PostPreview
+export default PostPreview;
